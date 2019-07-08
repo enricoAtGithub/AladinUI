a39fa64843c38c97dd992a0c4f07bb9d6e5dc9ad
kevin-schneider <kevin.schneider@simply4it.com>
2019-07-08 14:11:12 +0200

    added version info to footer

--- src/app/layout/app-footer/app.footer.component.ts
+++ src/app/layout/app-footer/app.footer.component.ts
@@ -1,9 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AppConfig, ServerInfo, UIInfo } from 'src/app/shared/app-config';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './app.footer.component.html'
 })
-export class AppFooterComponent {
+export class AppFooterComponent implements OnInit {
+  appDetails: string;
 
+  constructor(private appConfig: AppConfig) {}
+
+  ngOnInit() {
+    const uiInfo: UIInfo = AppConfig.getUIInfo();
+    const uiDetails = 'UI version=' + uiInfo.version + '.' + uiInfo.git_branch + '.' + uiInfo.build_no + '.' + uiInfo.git_sha;
+    this.appDetails = uiDetails + ', loading BE details....';
+    this.appConfig.serverInfo( (serverInfo: ServerInfo) => {
+      this.appDetails = uiDetails + ', BE host=' + serverInfo.host + ', BE version=' + serverInfo.version;
+    } );
+  }
 }
