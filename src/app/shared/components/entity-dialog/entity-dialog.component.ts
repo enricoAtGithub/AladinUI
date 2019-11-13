732cac8d0e8eed4914400f0f51886c355a18a4ae
kevin-schneider <kevin.schneider@simply4it.com>
2019-11-13 21:50:24 +0100

    cleanup
    (cherry picked from commit ed4beab6dfb5e7d6a200136cfd4cd47fe9c49e63)

--- src/app/shared/components/entity-dialog/entity-dialog.component.ts
+++ src/app/shared/components/entity-dialog/entity-dialog.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { DynamicDialogRef, DynamicDialogConfig, InputText, InputTextModule } from 'primeng/primeng';
 import { EntityConfiguration } from '../../models/entity-configuration';
-import { FormGroup, ControlContainer, NgForm } from '@angular/forms';
+import { FormGroup, NgForm } from '@angular/forms';
 import { EntityService } from '../../services/entity.service';
 import { CatalogueService } from 'src/app/user/services/catalogue.service';
 import { TableData } from '../../models/table-data';
-import { Observable, Subject } from 'rxjs';
+import { Subject } from 'rxjs';
 import { first } from 'rxjs/operators';
 
 @Component({
@@ -45,7 +45,7 @@ export class EntityDialogComponent implements OnInit {
     });
   }
 
-  openEntitySelectionDialog(field: any, form: NgForm, inn: any) {
+  openEntitySelectionDialog(field: any, form: NgForm) {
     this.entitySelectionTableData = new TableData(field['type'], field['type'], false, false, false, true, true, undefined, '700px', false);
     this.displayEntitySelectionDialog = true;
     this.$selectedEntity.pipe(first()).subscribe(selectedId => {
