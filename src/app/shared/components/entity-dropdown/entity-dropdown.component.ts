6d79f81e19a0b9ff03ba55a4544c9fc95fb5ba99
Maik Schönfeld <maik.schoenfeld@simply4it.de>
2019-11-22 17:50:29 +0100

    finished entity-dropdown

--- src/app/shared/components/entity-dropdown/entity-dropdown.component.ts
+++ src/app/shared/components/entity-dropdown/entity-dropdown.component.ts
@@ -10,28 +10,12 @@ import { EntityService } from '../../services/entity.service';
 })
 export class EntityDropdownComponent implements OnInit {
 
-
-/*
- * structure:
- *  get entity-data
- *    filter seems to be the only feasible option. todo: ask enrico
- *  map data element to defined fieldname
- *
- * set output value
- *
- * how to make it more dynamic
- *  selected option doesn't have to be string. could be a number too. or a combined element
- *    (but then the dynamic table dialog like in the entity-dialog component would be more feasible.)
- */
-
-  // @Input() optionLabel = 'Bitte Auswählen';
   @Input() propertyName: string;
   @Input() fieldName: string;
   @Input() allowNull = true;
-  @Input() selectedOption = '';
-  @Input() selectedOptionId: number;
-  @Output() selectedOptionChange = new EventEmitter();
-  @Output() selectedOptionIdChange = new EventEmitter();
+  @Input() selectedOptionId?: number;
+  @Output() selectedOptionChanged = new EventEmitter();
+  @Output() selectedOptionIdChanged = new EventEmitter();
 
   selectItems: SelectItem[];
   selectedItem: any;
@@ -40,35 +24,35 @@ export class EntityDropdownComponent implements OnInit {
   constructor(private entityService: EntityService) { }
 
   ngOnInit() {
-    console.log('[EntityDropdown-ngOnInit] propertyName: ', this.propertyName);
-    console.log('[EntityDropdown-ngOnInit] fieldName: ', this.fieldName);
     this.getEntityData();
   }
 
   selectedItemChanged(event: any): void {
-    console.log('[EntityDropdown-selectedItemChanged] event: ', event);
-    console.log('[EntityDropdown-selectedItemChanged] event.value: ', event.value);
-    this.selectedOptionChange.emit(event.value);
+    // console.log('[EntityDropdown-selectedItemChanged] event: ', event);
+    // console.log('[EntityDropdown-selectedItemChanged] event.value: ', event.value[this.fieldName]);
+    this.selectedOptionChanged.emit(event.value[this.fieldName]);
+    this.selectedOptionIdChanged.emit(event.value['id']);
   }
 
   getEntityData(): void {
-    console.log('[EntityDropdown-getEntityData] start: ');
     this.selectItems = [];
     this.entityService
           .filter(this.propertyName, 1, 100, '', '')
           .subscribe(
             entityData => {
-              console.log('[EntityDropdown-getEntityData] entityData: ', entityData);
               this.data = entityData;
               const tempSelectItems: SelectItem[] = [];
               tempSelectItems.push({label: 'Kein Lieferant', value: null});
               this.selectItems = tempSelectItems.concat(this.data.data.map(entity => {
-                  const selectItem = <SelectItem>{label: entity[this.fieldName], value: entity[this.fieldName]};
-                  console.log('[EntityDropdown-getEntityData] selectItem: ', selectItem);
+                  const selectItem = <SelectItem>{label: entity[this.fieldName], value: entity};
                   return selectItem;
                 }));
-              // this.selectItems = tempSelectItems;
-              console.log('[EntityDropdown-getEntityData] selectItems: ', this.selectItems);
+
+              if (!!this.selectedOptionId) {
+                this.selectedItem = entityData.data.find(data => data['id'] === this.selectedOptionId);
+              } else {
+                this.selectedItem = tempSelectItems[0].value;
+              }
             }
           );
   }
