88d8b2ea518544fe490ffdfb43f98e7201120142
Maik Schönfeld <maik.schoenfeld@simply4it.de>
2020-02-08 18:44:23 +0100

    extended file upload dialog
    picked from:
    
    Commit: 1baf7d6df2f52842fd8f963ac1c8d8c4cf258571 [1baf7d6]
    Parents: 7710c4cdea
    Author: Maik Schönfeld <maik.schoenfeld@simply4it.de>
    Date: Montag, 22. Juli 2019 16:07:49
    Committer: Maik Schönfeld
    extended file upload dialog

--- src/app/shared/components/file-upload-dialog/file-upload-dialog.component.ts
+++ src/app/shared/components/file-upload-dialog/file-upload-dialog.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-file-upload-dialog',
@@ -7,9 +7,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FileUploadDialogComponent implements OnInit {
 
+  @Input() chooseLabel: string;
+  @Input() uploadLabel: string;
+  @Input() cancelLabel: string;
+  url: string;
+
   constructor() { }
 
   ngOnInit() {
   }
+
+  onBeforeUpload(event: any) {
+
+  }
+
+  onSend(event: any) {
+    console.log('onSend');
+  }
+
+  onUpload(event: any) {
+    console.log('onUpload');
+  }
+
+  onError(event: any) {
+    console.log('onError');
+  }
+
+  onClear(event: any) {
+    console.log('onClear');
+  }
+
+  onRemove(event: any) {
+    console.log('onRemove');
+  }
+
+  onSelect(event: any) {
+    console.log('onSelect');
+  }
+
+  onProgress(event: any) {
+    console.log('onProgress');
+  }
+
+
+
+
+
 
 }
