1f5e6f90a401ead1b642d16992a2d27a192194fe
Maik Schönfeld <maik.schoenfeld@simply4it.de>
2020-02-21 17:25:51 +0100

    changed appConfig to skip http call to read postConfig.json
    picked from:
    
    Commit: 0d35620dd69c48111cf04ed8f334514636ce1f7a [0d35620]
    Parents: 76a0297b4f
    Author: Maik Schönfeld <maik.schoenfeld@simply4it.de>
    Date: Mittwoch, 7. August 2019 08:23:28
    Committer: Maik Schönfeld
    changed appConfig to skip http call to read postConfig.json

--- src/app/shared/app-config.ts
+++ src/app/shared/app-config.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment';
 import { BehaviorSubject, Subject } from 'rxjs';
+import PostBuildConfig from '../../assets/config/postbuildconfig.json';
 
 export class ServerInfo {
     host: string;
@@ -23,7 +24,9 @@ export class UIInfo {
 @Injectable()
 export class AppConfig {
 
-    constructor(private http: HttpClient) {}
+    constructor(private http: HttpClient) {
+        this.load();
+    }
 
     static uiInfo: UIInfo;
 
@@ -36,27 +39,38 @@ export class AppConfig {
     }
 
     load() {
-        const jsonFile = `assets/config/postbuildconfig.json`;
-        return new Promise<void>((resolve, reject) => {
-            this.http.get(jsonFile).toPromise().then((response: UIInfo) => {
-                AppConfig.uiInfo = response;
+        // const jsonFile = `assets/config/postbuildconfig.json`;
+        console.log('[AppConfig-load] loading config...');
+        AppConfig.uiInfo = PostBuildConfig as UIInfo;
+        if (!AppConfig.uiInfo.baseUrl) {
+            console.log('take Backend URL from environment');
+            AppConfig.uiInfo.baseUrl = environment.baseUrl;
+        } else {
+            console.log('take Backend URL from postbuildConfig');
+        }
+        // return new Promise<void>((resolve, reject) => {
+        //     this.http.get(jsonFile).toPromise().then((response: UIInfo) => {
+        //         console.log('[AppConfig-load] reading config. uiInfo: ', response);
+        //         AppConfig.uiInfo = response;
 
-                if (!AppConfig.uiInfo.baseUrl) {
-                    console.log('take Backend URL from environment');
-                    AppConfig.uiInfo.baseUrl = environment.baseUrl;
-                } else {
-                    console.log('take Backend URL from postbuildConfig');
-                }
+        //         if (!AppConfig.uiInfo.baseUrl) {
+        //             console.log('take Backend URL from environment');
+        //             AppConfig.uiInfo.baseUrl = environment.baseUrl;
+        //         } else {
+        //             console.log('take Backend URL from postbuildConfig');
+        //         }
 
-                resolve();
-            }).catch((response: any) => {
-               reject(`Could not load file '${jsonFile}': ${JSON.stringify(response)}`);
-            });
-        });
+        //         resolve();
+        //     }).catch((response: any) => {
+        //        reject(`Could not load file '${jsonFile}': ${JSON.stringify(response)}`);
+        //     });
+        // });
     }
 
     serverInfo(myFunc) {
         return this.http.get<ServerInfo>(AppConfig.getBaseUrl() + '/admin/info').subscribe(myFunc);
     }
+
+
 
 }
