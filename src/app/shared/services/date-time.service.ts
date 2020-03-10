1cadc0f948efcf17655ebeaeedd9f046120c5333
Uwe Gaertner <uwe.gaertner@pergocon.de>
2020-03-10 23:04:15 +0100

    Updat date-time service. Change methods to static.

--- src/app/shared/services/date-time.service.ts
+++ src/app/shared/services/date-time.service.ts
@@ -8,6 +8,17 @@ export class DateTimeService {
   readonly CALENDAR_DATE_FORMAT = 'dd.mm.yy';
 
   constructor() { }
+  public static convertApiDateTimeStringToDate = function (apiDateString: string): Date {
+    if (!apiDateString) {
+      return null;
+    }
+    let [datePart, timePart] = apiDateString.split('T');
+    const dateSubParts = datePart.split('-');
+    const timeSubParts = timePart.split(':');
+
+    // this is asking for trouble. todo: switch date management to moment.js
+    return new Date(+dateSubParts[0], +dateSubParts[1] - 1, +dateSubParts[2], +timeSubParts[0], +timeSubParts[1], +timeSubParts[2]);
+  }
 
   /**
    * Converts a date object in to the text format expected by the server.
@@ -18,45 +29,38 @@ export class DateTimeService {
    * @returns {string} The converted date string in the format: 'yyyy-MM-ddThh:mm:ss'.
    * @memberof DateTimeService
    */
-  convertDateToApiConformTimeString(date: Date): string {
+  public static convertDateToApiConformTimeString = function (date: Date): string {
 
     if (!date) {
       return null;
     }
 
-    const seconds = this.addZero(date.getSeconds());
-    const minutes = this.addZero(date.getMinutes());
-    const hours = this.addZero(date.getHours());
-    const days = this.addZero(date.getDate());
-    const month = this.addZero(date.getMonth() + 1); // .getMonth is zero-based.
-    const year = this.addZero(date.getFullYear());
+    const seconds = DateTimeService.addZero(date.getSeconds());
+    const minutes = DateTimeService.addZero(date.getMinutes());
+    const hours = DateTimeService.addZero(date.getHours());
+    const days = DateTimeService.addZero(date.getDate());
+    const month = DateTimeService.addZero(date.getMonth() + 1); // .getMonth is zero-based.
+    const year = DateTimeService.addZero(date.getFullYear());
 
     const result = `${year}-${month}-${days}T${hours}:${minutes}:${seconds}`;
 
     return result;
+  };
+
+  public static addZero = function (n: number): string {
+    return n < 10 ? `0${n}` : `${n}`;
   }
 
   convertDateStringToApiConformTimeString(dateString: string): string {
+
     const dateComponents = dateString.split('.');
 
     // this is asking for trouble. todo: switch date management to moment.js
     const date = new Date(+dateComponents[2], +dateComponents[1] - 1, +dateComponents[0]);
-    return this.convertDateToApiConformTimeString(date);
+    return DateTimeService.convertDateToApiConformTimeString(date);
   }
 
-  public static convertApiDateTimeStringToDate = function (apiDateString: string): Date {
-    if (!apiDateString) {
-      return null;
-    }
-    var [datePart, timePart] = apiDateString.split('T')
-    const dateSubParts = datePart.split('-');
-    const timeSubParts = timePart.split(':');
-    //console.log(dateSubParts)
-    //console.log(timeSubParts)
 
-    // this is asking for trouble. todo: switch date management to moment.js
-    return new Date(+dateSubParts[0], +dateSubParts[1] - 1, +dateSubParts[2], +timeSubParts[0], +timeSubParts[1], +timeSubParts[2]);
-  }
 
   convertApiDateTimeStringToCalendarString(apiDateString: string): string {
     if (!apiDateString) {
@@ -74,7 +78,4 @@ export class DateTimeService {
     return result;
   }
 
-  private addZero(n: number): string {
-    return n < 10 ? `0${n}` : `${n}`;
-  }
 }
