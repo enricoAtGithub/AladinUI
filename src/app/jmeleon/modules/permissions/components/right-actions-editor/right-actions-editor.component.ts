552127ebe1f65ddecffe2dfa49712797dcfe6298
Maik Schönfeld <maik.schoenfeld@simply4it.de>
2020-04-02 18:43:38 +0200

    added update-flag for selected area

--- src/app/jmeleon/modules/permissions/components/right-actions-editor/right-actions-editor.component.ts
+++ src/app/jmeleon/modules/permissions/components/right-actions-editor/right-actions-editor.component.ts
@@ -26,6 +26,7 @@ export class RightActionsEditorComponent implements OnInit, OnChanges, OnDestroy
   selectedSection: any;
 
   expandingOrCollapsingTreeIsActive: boolean;
+  updatingSelectedAreaActive: boolean;
 
   constructor(private facade: JmeleonActionsFacadeService) { }
 
@@ -58,35 +59,41 @@ export class RightActionsEditorComponent implements OnInit, OnChanges, OnDestroy
 
 
   nodeSelect(event) {
-    // console.log('selected node: ', event.node);
+    console.log('selected node: ', event.node);
+    // this.updatingSelectedAreaActive = true;
     this.facade.addActionToRight(this.entryId, event.node, this.selectedActions);
 
   }
   nodeUnselect(event) {
     // console.log('unselected node: ', event.node);
+    // this.updatingSelectedAreaActive = true;
     this.facade.removeActionFromRight(this.entryId, event.node, this.selectedActions);
 
   }
 
   sectionSelected(event) {
-    // console.log('selection changed: ', event);    
+    // console.log('selection changed: ', event);
+    // if (this.updatingSelectedAreaActive) {
+    //   this.updatingSelectedAreaActive = false;
+    //   return;
+    // }
     this.facade.selectSection(event.value.key);
   }
 
-  expandTrees(trees: TreeNode[]):void{
+  expandTrees(trees: TreeNode[]): void {
     this.expandingOrCollapsingTreeIsActive = true;
     trees.forEach(tree => JMeleonActionTreeUtils.expandOrCollapseTree(tree));
   }
 
-  collapseTrees(trees: TreeNode[]):void{
+  collapseTrees(trees: TreeNode[]): void {
     this.expandingOrCollapsingTreeIsActive = true;
     trees.forEach(tree => JMeleonActionTreeUtils.expandOrCollapseTree(tree, false));
   }
 
-  nodeExpanded(event):void{
+  nodeExpanded(event): void {
     // console.log('expanded: ', event);
   }
-  nodeCollapsed(event):void{
+  nodeCollapsed(event): void {
     // console.log('collapsed: ', event);
 
   }
