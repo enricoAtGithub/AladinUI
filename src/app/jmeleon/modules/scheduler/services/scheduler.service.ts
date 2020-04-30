c2d01094286d2ec8b2ae615712465f23b4dcee73
Uwe Gaertner <uwe.gaertner@pergocon.de>
2020-04-30 14:30:40 +0200

    consider "type" in scheduler service

--- src/app/jmeleon/modules/scheduler/services/scheduler.service.ts
+++ src/app/jmeleon/modules/scheduler/services/scheduler.service.ts
@@ -36,7 +36,7 @@ export class SchedulerService {
 
           return {
             Id: 'order#' + schEv.id,
-            Type: 'order',
+            Type: schEv.type,
             RefId: schEv.id,
             Subject: schEv.subject,
             StartTime: startDateTime,
@@ -73,7 +73,7 @@ export class SchedulerService {
 
               return {
                 Id: 'order#' + schEv.id,
-                Type: 'order',
+                Type: schEv.Type,
                 RefId: schEv.id,
                 Subject: schEv.subject,
                 Description: schEv.description,
