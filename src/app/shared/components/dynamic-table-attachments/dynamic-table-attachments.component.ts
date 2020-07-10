79e8a5ccd57e12c33e5be08c5f5b2b904aedcf4f
Uwe Gaertner <uwe.gaertner@pergocon.de>
2020-07-10 12:22:20 +0200

    rename mainId to entryId in dynamic table attachment component

--- src/app/shared/components/dynamic-table-attachments/dynamic-table-attachments.component.ts
+++ src/app/shared/components/dynamic-table-attachments/dynamic-table-attachments.component.ts
@@ -16,7 +16,7 @@ import { GroupConfiguration } from '../../models/group-configuration';
 })
 export class DynamicTableAttachmentsComponent implements OnInit, OnChanges {
   @Input() configName: string;
-  @Input() mainId: number;
+  @Input() entryId: number;
   @Input() excludedPanels: string[];
 
   isEmpty: boolean;
