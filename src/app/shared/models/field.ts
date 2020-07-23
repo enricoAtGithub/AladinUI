308f3ee809e1e940badb28659cc3fcdc7884394d
Uwe Gaertner <uwe.gaertner@pergocon.de>
2020-07-23 12:07:39 +0200

    move Python and json to primitiveTypes

--- src/app/shared/models/field.ts
+++ src/app/shared/models/field.ts
@@ -17,11 +17,11 @@ export class Field extends BasicFieldConfig {
     formula: string;
 
     public static isPrimitiveType(type: string) {
-        return type === 'String' || type === 'int' || type === 'boolean' || type === 'Date' || type === 'Currency' || type === 'float' ;
+        return type === 'String' || type === 'int' || type === 'boolean' || type === 'Date' || type === 'Currency' || type === 'float' || type === 'json' || type === 'python';
     }
 
     public static isKnownType(type: string) {
-        return this.isPrimitiveType(type)  || type === 'json' || type === 'python' || type === 'dtoType' || type === 'CatalogueEntry' || type === 'Icon';
+        return this.isPrimitiveType(type)  || type === 'dtoType' || type === 'CatalogueEntry' || type === 'Icon';
     }
 }
 
