185eb3c99d5fb1832a3fa2f4016477a7e28238b7
Enrico Quasdorf <enrico.quasdorf@gmail.com>
2020-07-27 11:01:21 +0200

    fix build scripts

--- jenkins/deploy_jenkins.sh
+++ jenkins/deploy_jenkins.sh
@@ -13,8 +13,8 @@ echo "Deploying $APP_NAME on $TOMCAT_SERVICE (Path: $TOMCAT_PATH, Port: $TOMCAT_
 
 sudo systemctl stop $TOMCAT_SERVICE
 
-rm $TOMCAT_PATH/webapps/${APP_NAME}.war
-cp target/${APP_NAME}*.war $TOMCAT_PATH/webapps/${APP_NAME}.war
+rm $TOMCAT_PATH/webapps/${APP_NAME}-ui.war
+cp target/${APP_NAME}*.war $TOMCAT_PATH/webapps/${APP_NAME}-ui.war
 
 sudo systemctl start $TOMCAT_SERVICE
 
