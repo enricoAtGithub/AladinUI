25c6aa20713b85e0e968cf7a3cbf74d857ef2d13
Uwe Gaertner <uwe.gaertner@pergocon.de>
2020-08-24 13:51:48 +0200

    increase max upload size to 10 MB for Martin

--- src/app/shared/components/file-upload-dialog/file-upload-dialog.component.ts
+++ src/app/shared/components/file-upload-dialog/file-upload-dialog.component.ts
@@ -60,7 +60,7 @@ export class FileUploadDialogComponent implements OnInit {
     this.ownerType = data['ownerType'];
 
 
-    this.setMaxUploadSize(1);
+    this.setMaxUploadSize(10);
     // this.fileId is set in case of updating an existing file => update; if not set => upload
     (this.fileId) ? this.url = this.fileService.getUpdateFileUrl() : this.url = this.fileService.getUploadUrl();
 
