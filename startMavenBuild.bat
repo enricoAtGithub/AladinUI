308df0ca1c018a8a21906b6ed385996751c30618
Maik Schönfeld <maik.schoenfeld@simply4it.de>
2020-09-22 12:04:13 +0200

    extended maven build script with angular profile parameter

--- startMavenBuild.bat
+++ startMavenBuild.bat
@@ -1,2 +1,2 @@
-mvn --settings %USERPROFILE%/.m2/settings-simply.xml clean install -Dbuild_no=42 -Dgit_sha=42 -Dgit_branch=42
+mvn --settings %USERPROFILE%/.m2/settings-simply.xml clean install -Dbuild_no=42 -Dgit_sha=42 -Dgit_branch=42 -Dangular_profile=production
 git checkout src/assets/config/postbuildconfig.json
